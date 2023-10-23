cb74c76a40c5bd3e4d2c935925caa3d1b45c498e
Meshack Korir <meshackkorir100@gmail.com>
2023-10-23 13:41:53 +0300

    properly

--- index.js
+++ index.js
@@ -10,7 +10,45 @@ function changeTopic(topic) {
     h2.textContent = topic
     h2.style.color = "blue"
     let form = document.querySelector("#record")
-        
+
 }
 
 changeTopic("Thursday Session")
+
+function collectData() {
+    // collecting data from form
+
+    let form = document.querySelector("#record")
+    form.addEventListener("submit", (event) => {
+       event.preventDefault() 
+        // logic
+
+        let formData = {
+            fullName: event.target.name.value,
+            email: event.target.email.value,
+            password: event.target.password.value
+        }
+
+    });
+};
+
+collectData();
+
+function displayFeedback() {
+    // grab form
+    let data = document.querySelector(".feedbackForm")
+    data.addEventListener("submit", (e) => {
+        e.preventDefault()
+
+        //grab the input value
+        let feedbackInput = e.target.feedback.value
+        let p = document.createElement('p')
+        p.textContent = feedbackInput
+        let section = document.querySelector('section')
+        section.appendChild(p)
+        
+    })
+
+}
+
+displayFeedback();
